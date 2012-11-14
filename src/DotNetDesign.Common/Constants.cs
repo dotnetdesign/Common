1f4c7a2ced8d18a10bb3f5258d74c44d12c1d554
Adam Carr <adam.carr@gmail.com>
2012-11-13 19:55:44 -0800

    Adding regular expression constants

---
+++ src/DotNetDesign.Common/Constants.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetDesign.Common
+{
+    /// <summary>
+    /// Constants
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// Regular expressions
+        /// </summary>
+        public static class RegExs
+        {
+            /// <summary>
+            /// The email address regular expression.
+            /// </summary>
+            public const string Email = @"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*";
+
+            /// <summary>
+            /// Regular Expression used to enforce a strong password.
+            /// </summary>
+            public const string StrongPassword = @"^(?=.*[A-Z])(?=.*[a-z])(?=.*[@#$%^&+=]|.*\d)^.{8,32}$";
+        }
+    }
+}
