8c14511259baa4a297d86d966de12b9447f41155
Adam Carr <adam.carr@gmail.com>
2013-05-18 17:16:08 -0700

    Check-in from visualstudio.com

--- src/DotNetDesign.Common.Tests/GuardTests.cs
+++ src/DotNetDesign.Common.Tests/GuardTests.cs
@@ -1,91 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Moq;
-using Xunit;
-using Xunit.Extensions;
-using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DotNetDesign.Common.Tests
 {
-    public class GuardTests
-    {
-        [Fact]
-        public void ArgumentNotNullGuardTests()
-        {
-            string nullString = null;
-            string nullStringName = "nullString";
-            string notNullString = "not null";
-            string notNullStringName = "notNullStringName";
-            IList<int> nullObject = null;
-            string nullObjectName = "nullObjectName";
+	[TestClass]
+	public class GuardTests
+	{
+		[TestMethod]
+		public void ArgumentNotNullGuardTests()
+		{
+			string nullString = null;
+			const string nullStringName = "nullString";
+			const string notNullString = "not null";
+			const string notNullStringName = "notNullStringName";
+			IList<int> nullObject = null;
+			const string nullObjectName = "nullObjectName";
 
-            Assert.Throws<ArgumentNullException>(() => Guard.ArgumentNotNull(nullString, nullStringName))
-                  .Message.Should().Contain(nullStringName);
-            Assert.Throws<ArgumentNullException>(() => Guard.ArgumentNotNull(nullObject, nullObjectName))
-                  .Message.Should().Contain(nullObjectName);
+			TestHelpers.TestException<ArgumentNullException>(() => Guard.ArgumentNotNull(nullString, nullStringName), nullStringName);
+			TestHelpers.TestException<ArgumentNullException>(() => Guard.ArgumentNotNull(nullObject, nullObjectName), nullObjectName);
 
-            Guard.ArgumentNotNull(nullString, nullStringName, false).Should().BeFalse();
-            Guard.ArgumentNotNull(nullObject, nullObjectName, false).Should().BeFalse();
+			Assert.IsFalse(Guard.ArgumentNotNull(nullString, nullStringName, false));
+			Assert.IsFalse(Guard.ArgumentNotNull(nullObject, nullObjectName, false));
 
-            Guard.ArgumentNotNull(notNullString, notNullStringName).Should().BeTrue();
-            Guard.ArgumentNotNull(notNullString, notNullStringName, false).Should().BeTrue();
-        }
+			Assert.IsTrue(Guard.ArgumentNotNull(notNullString, notNullStringName));
+			Assert.IsTrue(Guard.ArgumentNotNull(notNullString, notNullStringName, false));
+		}
 
-        [Fact]
-        public void ArgumentNotNullOrEmptyTests()
-        {
-            string nullString = null;
-            string nullStringName = "nullString";
-            string emptyString = string.Empty;
-            string emptyStringName = "emptyString";
-            string spacesString = "  ";
-            string spacesStringName = "spacesString";
-            string tabsString = "\t\t";
-            string tabsStringName = "tabsString";
-            string whitespaceString = "\t\t  \r\n\t  \t  \r\n";
-            string whitespaceStringName = "whitespaceString";
-            string notNullString = "not null";
-            string notNullStringName = "notNullStringName";
+		[TestMethod]
+		public void ArgumentNotNullOrEmptyTests()
+		{
+			var emptyString = string.Empty;
+			const string nullStringName = "nullString";
+			const string emptyStringName = "emptyString";
+			const string spacesString = "  ";
+			const string spacesStringName = "spacesString";
+			const string tabsString = "\t\t";
+			const string tabsStringName = "tabsString";
+			const string whitespaceString = "\t\t  \r\n\t  \t  \r\n";
+			const string whitespaceStringName = "whitespaceString";
+			const string notNullString = "not null";
+			const string notNullStringName = "notNullStringName";
 
-            Assert.Throws<ArgumentNullException>(() => Guard.ArgumentNotNullOrEmpty(nullString, nullStringName))
-                  .Message.Should().Contain(nullStringName);
-            Assert.Throws<ArgumentNullException>(() => Guard.ArgumentNotNullOrEmpty(nullString, nullStringName, true))
-                  .Message.Should().Contain(nullStringName);
-            Guard.ArgumentNotNullOrEmpty(nullString, nullStringName, false, false).Should().BeFalse();
-            Guard.ArgumentNotNullOrEmpty(nullString, nullStringName, true, false).Should().BeFalse();
+			TestHelpers.TestException<ArgumentNullException>(() => Guard.ArgumentNotNullOrEmpty(null, nullStringName), nullStringName);
+			TestHelpers.TestException<ArgumentNullException>(
+				() => Guard.ArgumentNotNullOrEmpty(null, nullStringName, true),
+				nullStringName);
+			Assert.IsFalse(Guard.ArgumentNotNullOrEmpty(null, nullStringName, false, false));
+			Assert.IsFalse(Guard.ArgumentNotNullOrEmpty(null, nullStringName, true, false));
 
-            Assert.Throws<ArgumentNullException>(() => Guard.ArgumentNotNullOrEmpty(emptyString, emptyStringName))
-                  .Message.Should().Contain(emptyStringName);
-            Assert.Throws<ArgumentNullException>(() => Guard.ArgumentNotNullOrEmpty(emptyString, emptyStringName, true))
-                  .Message.Should().Contain(emptyStringName);
-            Guard.ArgumentNotNullOrEmpty(emptyString, emptyStringName, false, false).Should().BeFalse();
-            Guard.ArgumentNotNullOrEmpty(emptyString, emptyStringName, true, false).Should().BeFalse();
+			TestHelpers.TestException<ArgumentNullException>(
+				() => Guard.ArgumentNotNullOrEmpty(emptyString, emptyStringName),
+				emptyStringName);
 
-            Assert.Throws<ArgumentNullException>(() => Guard.ArgumentNotNullOrEmpty(spacesString, spacesStringName))
-                  .Message.Should().Contain(spacesStringName);
-            Guard.ArgumentNotNullOrEmpty(spacesString, spacesStringName, true).Should().BeTrue();
-            Guard.ArgumentNotNullOrEmpty(spacesString, spacesStringName, false, false).Should().BeFalse();
-            Guard.ArgumentNotNullOrEmpty(spacesString, spacesStringName, true, false).Should().BeTrue();
+			TestHelpers.TestException<ArgumentNullException>(
+				() => Guard.ArgumentNotNullOrEmpty(emptyString, emptyStringName, true),
+				emptyStringName);
 
-            Assert.Throws<ArgumentNullException>(() => Guard.ArgumentNotNullOrEmpty(tabsString, tabsStringName))
-                  .Message.Should().Contain(tabsStringName);
-            Guard.ArgumentNotNullOrEmpty(tabsString, tabsStringName, true).Should().BeTrue();
-            Guard.ArgumentNotNullOrEmpty(tabsString, tabsStringName, false, false).Should().BeFalse();
-            Guard.ArgumentNotNullOrEmpty(tabsString, tabsStringName, true, false).Should().BeTrue();
+			Assert.IsFalse(Guard.ArgumentNotNullOrEmpty(emptyString, emptyStringName, false, false));
+			Assert.IsFalse(Guard.ArgumentNotNullOrEmpty(emptyString, emptyStringName, true, false));
 
-            Assert.Throws<ArgumentNullException>(() => Guard.ArgumentNotNullOrEmpty(whitespaceString, whitespaceStringName))
-                  .Message.Should().Contain(whitespaceStringName);
-            Guard.ArgumentNotNullOrEmpty(whitespaceString, whitespaceStringName, true).Should().BeTrue();
-            Guard.ArgumentNotNullOrEmpty(whitespaceString, whitespaceStringName, false, false).Should().BeFalse();
-            Guard.ArgumentNotNullOrEmpty(whitespaceString, whitespaceStringName, true, false).Should().BeTrue();
+			TestHelpers.TestException<ArgumentNullException>(
+				() => Guard.ArgumentNotNullOrEmpty(spacesString, spacesStringName),
+				spacesStringName);
+			Assert.IsTrue(Guard.ArgumentNotNullOrEmpty(spacesString, spacesStringName, true));
+			Assert.IsFalse(Guard.ArgumentNotNullOrEmpty(spacesString, spacesStringName, false, false));
+			Assert.IsTrue(Guard.ArgumentNotNullOrEmpty(spacesString, spacesStringName, true, false));
 
-            Guard.ArgumentNotNullOrEmpty(notNullString, notNullStringName).Should().BeTrue();
-            Guard.ArgumentNotNullOrEmpty(notNullString, notNullStringName, true).Should().BeTrue();
-            Guard.ArgumentNotNullOrEmpty(notNullString, notNullStringName, false, false).Should().BeTrue();
-            Guard.ArgumentNotNullOrEmpty(notNullString, notNullStringName, true, false).Should().BeTrue();
-        }
-    }
+			TestHelpers.TestException<ArgumentNullException>(
+				() => Guard.ArgumentNotNullOrEmpty(tabsString, tabsStringName),
+				tabsStringName);
+			Assert.IsTrue(Guard.ArgumentNotNullOrEmpty(tabsString, tabsStringName, true));
+			Assert.IsFalse(Guard.ArgumentNotNullOrEmpty(tabsString, tabsStringName, false, false));
+			Assert.IsTrue(Guard.ArgumentNotNullOrEmpty(tabsString, tabsStringName, true, false));
+
+			TestHelpers.TestException<ArgumentNullException>(
+				() => Guard.ArgumentNotNullOrEmpty(whitespaceString, whitespaceStringName),
+				whitespaceStringName);
+			Assert.IsTrue(Guard.ArgumentNotNullOrEmpty(whitespaceString, whitespaceStringName, true));
+			Assert.IsFalse(Guard.ArgumentNotNullOrEmpty(whitespaceString, whitespaceStringName, false, false));
+			Assert.IsTrue(Guard.ArgumentNotNullOrEmpty(whitespaceString, whitespaceStringName, true, false));
+			Assert.IsTrue(Guard.ArgumentNotNullOrEmpty(notNullString, notNullStringName));
+			Assert.IsTrue(Guard.ArgumentNotNullOrEmpty(notNullString, notNullStringName, true));
+			Assert.IsTrue(Guard.ArgumentNotNullOrEmpty(notNullString, notNullStringName, false, false));
+			Assert.IsTrue(Guard.ArgumentNotNullOrEmpty(notNullString, notNullStringName, true, false));
+		}
+	}
 }
--- src/DotNetDesign.Common/Constants.cs
+++ src/DotNetDesign.Common/Constants.cs
@@ -1,24 +1,24 @@
 ﻿namespace DotNetDesign.Common
 {
-    /// <summary>
-    /// Constants
-    /// </summary>
-    public static class Constants
-    {
-        /// <summary>
-        /// Regular expressions
-        /// </summary>
-        public static class RegExs
-        {
-            /// <summary>
-            /// The email address regular expression.
-            /// </summary>
-            public const string Email = @"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*";
+	/// <summary>
+	/// Constants
+	/// </summary>
+	public static class Constants
+	{
+		/// <summary>
+		/// Regular expressions
+		/// </summary>
+		public static class RegExs
+		{
+			/// <summary>
+			/// The email address regular expression.
+			/// </summary>
+			public const string Email = @"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*";
 
-            /// <summary>
-            /// Regular Expression used to enforce a strong password.
-            /// </summary>
-            public const string StrongPassword = @"^(?=.*[A-Z])(?=.*[a-z])(?=.*[@#$%^&+=]|.*\d)^.{8,32}$";
-        }
-    }
+			/// <summary>
+			/// Regular Expression used to enforce a strong password.
+			/// </summary>
+			public const string StrongPassword = @"^(?=.*[A-Z])(?=.*[a-z])(?=.*[@#$%^&+=]|.*\d)^.{8,32}$";
+		}
+	}
 }
